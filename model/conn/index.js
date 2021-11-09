4b2967ec9ab40bbdf2d9d91fbfe7722f89c0fc25
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-11-09 09:51:11 -0300

    ok

--- model/conn/index.js
+++ model/conn/index.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
 
 const Conn = async () => {
   await mongoose
-    .connect('mongodb+srv://pedro:12345@cluster0.8axru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => {
       console.log('MongoDB esta conectado');
     })
